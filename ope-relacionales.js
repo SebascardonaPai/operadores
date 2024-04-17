00f8aabc873ba4a94e3974891b5b2e7e90525802
sebastian cardona <juan.cardona227@asys.edu.co>
2024-04-16 20:16:25 -0500

    add comentarios relacionales

--- ope-relacionales.js
+++ ope-relacionales.js
@@ -1,23 +1,24 @@
 //igualdad
 let resulatdoigualdad = 10 =="10";
-console.log (resulatdoigualdad);
+console.log (`10 es igual a 10:${resulatdoigualdad}`);
 
 //desigualdad
 let resultadodesigualdad = 15 != "15";
-console.log(resultadodesigualdad);
+console.log(`15 es desigual que 15: ${resultadodesigualdad}`);
 
 //menor que 
 let resultadomenorque =8<12;
-console.log (resultadomenorque);
+console.log (`8 es menor que 12: ${resultadomenorque}`);
 
 //mayor que 
 let resultadomayorque = 20>10;
-console.log (resultadomayorque);
+console.log (`20 es mayor que 10: ${resultadomayorque}`);
 
 //menorigualque
 let resultadomenorigualque = 15<=15;
-console.log (resultadomenorigualque);
+console.log (`15 es menor o igual que 15: ${resultadomenorigualque}`);
 
 //mayorigualque 
 let resultadomayorigualque = 22>=20;
-console.log (resultadomayorigualque);
+console.log (`22 es mayor o igual que 20: ${resultadomayorigualque}`);
+
