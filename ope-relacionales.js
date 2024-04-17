dbb604191862ff6c8c73f008e836d273baa81d57
sebastian cardona <juan.cardona227@asys.edu.co>
2024-04-16 20:04:57 -0500

    add relacionales

---
+++ ope-relacionales.js
@@ -0,0 +1,23 @@
+//igualdad
+let resulatdoigualdad = 10 =="10";
+console.log (resulatdoigualdad);
+
+//desigualdad
+let resultadodesigualdad = 15 != "15";
+console.log(resultadodesigualdad);
+
+//menor que 
+let resultadomenorque =8<12;
+console.log (resultadomenorque);
+
+//mayor que 
+let resultadomayorque = 20>10;
+console.log (resultadomayorque);
+
+//menorigualque
+let resultadomenorigualque = 15<=15;
+console.log (resultadomenorigualque);
+
+//mayorigualque 
+let resultadomayorigualque = 22>=20;
+console.log (resultadomayorigualque);
